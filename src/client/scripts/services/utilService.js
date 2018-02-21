d520d936af46b5b758cb505e2780b39d3ea4275b
bhavyasaggi <bhavya.saggi@gmail.com>
2018-02-21 13:18:42 +0530

    tts

--- src/client/scripts/services/utilService.js
+++ src/client/scripts/services/utilService.js
@@ -1,1 +1,7 @@
 import localStorageService from "scripts/services/localStorageService"
+
+export function talkToMe(text){
+	var msg = new SpeechSynthesisUtterance();
+	msg.text = text || 'Did you miss me?';
+	speechSynthesis.speak(msg);
+}
