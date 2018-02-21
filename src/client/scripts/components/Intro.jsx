2bee1aa2d6315c10f036eb35caed8b3dee49b08e
Nimit Mangal <nimit.mangal@proptiger.com>
2018-02-21 23:07:28 +0530

    intro screen

--- src/client/scripts/components/Intro.jsx
+++ src/client/scripts/components/Intro.jsx
@@ -11,7 +11,20 @@ class Intro extends Component {
     const cls = cs('button', this.props.className)
     return (
       <div>
-        This is intro
+        <div className="sllr-info-wrap">
+          <div className="sllr-head intro">
+            <div className="sllr-img-wrap"></div>
+          </div>
+          <div className="sllr-name">John Mathewz</div>
+          <div className="sllr-loc">Rai Realtors, Gurgaon</div>
+        </div>
+
+        <div className="progress-wrap ta-c">
+          <div className="txt">Initializing...</div>
+        </div>
+
+        <div className="logo"></div>
+        <div className="footer"></div>
       </div>
     )
   }
--- src/client/scripts/components/similarCard.jsx
+++ src/client/scripts/components/similarCard.jsx
@@ -7,13 +7,14 @@ class SimilarCard extends Component {
         <div className="propcard flexrow">
             <div className="img-wrap" style={{backgroundImage:'url('+this.props.imgurl+')'}}></div>
             <div className="prop-content">
-            <div><span className="title">{this.props.title}</span> in {this.props.location}</div>
-            <div className="ltxt">{this.props.locality}</div>
-            <div className="price">&#8377; {this.props.price}</div>
+                <div><span className="title">{this.props.title}</span> in {this.props.location}</div>
+                <div className="ltxt">{this.props.locality}</div>
+                <div className="price">&#8377; {this.props.price}</div>
             </div>
         </div>
         <div className="btn-wrap flexrow">
-
+            <span className="btn"><i className="fa fa-thumbs-down"></i> Nopes</span>
+            <span className="btn"><i className="fa fa-heart-o"></i> Like</span>
         </div>
     </div>
     )
