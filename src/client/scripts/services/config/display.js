51e82bf4ac6b9cbe0d88ba7866962896179d89fe
bhavyasaggi <bhavya.saggi@gmail.com>
2018-02-21 19:28:29 +0530

    stepped Up config

--- src/client/scripts/services/config/display.js
+++ src/client/scripts/services/config/display.js
@@ -4,17 +4,14 @@ const options = {
   [STEPS["STEP_1"]]: {
     type: 'text',
     options: [{
-      displayText: 'Hey Mark, whats up?',
+      displayText: 'Hi, sorry I couldn’t pick your call.',
       id: 1
     }, {
-      displayText: 'Hey man, whats up?',
+      displayText: 'I saw that you have shown interest for rent in DLF Park Place',
       id: 2
     }, {
-      displayText: 'good good',
+      displayText: 'How may I further assist you today Mark?',
       id: 3
-    }, {
-      displayText: 'Hey Mark, whats up?',
-      id: 4
     }, {
       type: 'property-card',
       data: {
@@ -25,13 +22,94 @@ const options = {
     }]
   },
   [STEPS["STEP_2"]]: {
-    type: 'map'
+    type: 'text',
+    options: [{
+      displayText: 'This apartment is situated in one of the most sought after luxury societies in Gurgaon by DLF.',
+      id: 1
+    }, {
+      displayText: 'It is a meticulously designed 4BHK-Apartment with cut edge innovative interiors and facilities.',
+      id: 2
+    }, {
+      displayText: 'Located off just 200 meters from Golf course road, provides great connectivity.',
+      id: 3
+    }]
   },
   [STEPS["STEP_3"]]: {
-    type: 'text'
+    type: 'text',
+    options: [{
+      displayText: 'Sure, I can look that up.',
+      id: 1
+    },{
+      displayText: 'Is your budget of 85K a bit flexible on the higher side?',
+      id: 2
+    }]
   },
   [STEPS["STEP_4"]]: {
-    type: 'text'
+    type: 'text',
+    options: [{
+      displayText: 'There are 2 more properties near buy that you can check out',
+      id: 1
+    },{
+      displayText: 'Tell me which ones you like',
+      id: 2
+    }]
+  },
+  [STEPS["STEP_5"]]: {
+    type: 'text',
+    options: [{
+      displayText: 'Thank you.',
+      id: 1
+    },{
+      displayText: 'Would you like to schedule a visit to these properties?',
+      id: 2
+    }]
+  },
+  [STEPS["STEP_6"]]: {
+    type: 'text',
+    options: [{
+      displayText: 'Cool.',
+      id: 1
+    },{
+      displayText: 'I am free this saturday afternoon and sunday afternoon.',
+      id: 2
+    },{
+      displayText: 'Which one suits you better?',
+      id: 3
+    }]
+  },
+  [STEPS["STEP_7"]]: {
+    type: 'text',
+    options: [{
+      displayText: 'UNCHARTED TERRITORY! HOW DID YOU FIND ME?',
+      id: 1
+    }]
+  },
+  [STEPS["STEP_8"]]: {
+    type: 'map'
+  },
+  [STEPS["STEP_9"]]: {
+    type: 'text',
+    options: [{
+      displayText: 'UNCHARTED TERRITORY! HOW DID YOU FIND ME?',
+      id: 1
+    }]
+  },
+  [STEPS["STEP_10"]]: {
+    type: 'text',
+    options: [{
+      displayText: 'It was great talking to you Mark.',
+      id: 1
+    }, {
+      displayText: 'Looking forward!',
+      id: 2
+    }]
+  },
+  [STEPS["STEP_11"]]: {
+    type: 'text',
+    options: [{
+      displayText: 'Is this the real life?',
+      id: 1
+    }]
   }
 }
 
--- src/client/scripts/services/config/options.js
+++ src/client/scripts/services/config/options.js
@@ -1,36 +1,123 @@
 import STEPS from 'scripts/services/config/steps'
 
+/*
+STEP 0 -> Loading
+STEP 1 -> Intro
+STEP 2 -> Know more
+STEP 3 -> Budget
+STEP 4 -> Similar 
+STEP 5 -> Schedule
+STEP 6 -> Time Select
+STEP 7 -> Pickup Time
+STEP 8 -> MAP
+STEP 9 -> Pickup Location
+STEP 10 -> Thanks
+STEP 11 -> Kill Me PLEASE
+*/
+
 const commonOptions = [{
-  displayText: "Common option",
+  displayText: "What were we talking about?",
   nextStep: STEPS["STEP_1"]
 }]
 
 const options = {
   [STEPS["STEP_1"]]: [
-    ...commonOptions,
     {
-      displayText: 'step 1',
+      displayText: 'Know more on the property.',
       nextStep: STEPS["STEP_2"]
     },{
-      displayText: 'step 2'
+      displayText: 'Know more about DLF Park Place.'
+    }, {
+      displayText: 'When can I visit the property?'
     }
   ],
   [STEPS["STEP_2"]]: [
     ...commonOptions,
     {
-      displayText: 'step 2'
+      displayText: 'Any other similar properties nearby?',
+      nextStep: STEPS["STEP_3"]
+    }, {
+      displayText: 'Who is the owner?'
+    }, {
+      displayText: 'When can I visit the property?'
     }
   ],
   [STEPS["STEP_3"]]: [
     ...commonOptions,
     {
-      displayText: 'step 3'
+      displayText: 'I can go upto 1Lac if required.',
+      nextStep: STEPS["STEP_4"]
+    }, {
+      displayText: 'No, that is my upper limit'
     }
   ],
   [STEPS["STEP_4"]]: [
     ...commonOptions,
     {
-      displayText: 'step 4'
+      displayText: 'Pikachu, I choose you.',
+      nextStep: STEPS["STEP_5"]
+    }, {
+      displayText: 'NO.'
+    }
+  ],
+  [STEPS["STEP_5"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Yes definitely',
+      nextStep: STEPS["STEP_6"]
+    }, {
+      displayText: 'Umm..Not sure yet'
+    }
+  ],
+  [STEPS["STEP_6"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Saturday afternoon',
+      nextStep: STEPS["STEP_8"]
+    }, {
+      displayText: 'Sunday afternoon.'
+    }, {
+      displayText: 'Some other time.'
+    }
+  ],
+  [STEPS["STEP_7"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Seems that I am lost, show me da wey?',
+      nextStep: STEPS["STEP_1"]
+    }, {
+      displayText: 'NO.'
+    }
+  ],
+  [STEPS["STEP_8"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Yes definitely.',
+      nextStep: STEPS["STEP_10"]
+    }, {
+      displayText: 'Let me get back to you soon.'
+    }
+  ],
+  [STEPS["STEP_9"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Seems that I am lost, show me da wey?',
+      nextStep: STEPS["STEP_1"]
+    }, {
+      displayText: 'NO.'
+    }
+  ],
+  [STEPS["STEP_10"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Thanks, bye!',
+      nextStep: STEPS["STEP_11"]
+    }
+  ],
+  [STEPS["STEP_11"]]: [
+    ...commonOptions,
+    {
+      displayText: 'Is it a fantasy?'
     }
   ]
 }
