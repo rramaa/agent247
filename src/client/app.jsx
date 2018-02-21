8501f94b9fc28f79d2832700d54ebe9f4fd4af44
Ramakrishna <ramakrishna@proptiger.com>
2018-02-21 11:48:20 +0530

    2nd commit

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react"
 import { render } from "react-dom"
-import Sidebar from "scripts/containers/Sidebar"
-import MainContent from "scripts/containers/MainContent"
 import localStorageService from "scripts/services/localStorageService"
 import style from "style/style"
 import configureStore from 'scripts/services/store'
@@ -10,7 +8,6 @@ import { Provider, connect } from 'react-redux'
 let store = configureStore()
 
 class ToDo extends Component {
-
     constructor(props) {
         super(props)
     }
