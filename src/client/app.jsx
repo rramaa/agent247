ed7fbd7844b366851db46c9c121bd5d8e32de1dc
Nimit Mangal <nimit.mangal@proptiger.com>
2018-02-21 17:06:55 +0530

    ux changes

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -16,26 +16,38 @@ class ToDo extends Component {
         return (
             <div className="body-container">
 
-                {/* here goes the seller information */}
+                {/* Seller information details starts */}
                 <div class="sllr-info-wrap">
                     <div className="sllr-head speak">
-                        <i className="fa fa-volume-off"></i>
+                        <i className="fa fa-volume-up"></i>
                         <div className="sllr-img-wrap"></div>
                     </div>
                     <div className="sllr-name">John Mathewz</div>
                     <div className="sllr-loc">Rai Realtors, Gurgaon</div>
                 </div>
+                {/* Seller information details ends */}
 
-                {/* here goes the seller information */}
+                {/* Seller's Contextual Statements starts */}
                 <div className="sllr-statement-wrap">
                     <div className="statement">Hi Mark, Sorry couldn't pick up your call.</div>
                     <div className="statement">I saw that you have shown interest for rent in DLF Park Place.</div>
                     <div className="statement">
                         <div className="card propcard">
                             <div><span className="title">4 BHK Apartment</span> in DLF Park Place</div>
+                            <div class="ltxt">Sector 54, Gurgaon</div>
                         </div>
                     </div>
+                    <div className="statement">How may I further assist you today?</div>
                 </div>
+                {/* Seller's Contextual Statements ends */}
+
+                {/* Buyer options starts */}
+                <div className="options-wrap">
+                    <div className="btn mb10">Know more on the property</div>
+                    <div className="btn mb10">Know more about DLF Park Place</div>
+                    <div className="btn mb10">When can I visit the property?</div>
+                </div>
+                {/* Buyer options ends */}
             </div>
         )
     }
